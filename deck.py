71171e7dc982676b9d087d9092f0e094de707943
turnipsoup <jeremy@jeremyheckt.com>
2021-04-18 12:18:11 -0700

    single card info finished, added exception handling+logging

--- deck.py
+++ deck.py
@@ -1,5 +1,5 @@
 from src import library, librarian, painter, card, mage
-import sqlite3, os, logging, json, discord
+import sqlite3, os, logging, json, discord, logging
 
 # Load config file
 config_dir = './config'
@@ -7,9 +7,13 @@ config = json.loads(open(f"{config_dir}/config.json", "r").read())
 discord_api_token = open(f'{config_dir}/bot_token.token').read()
 
 logging.basicConfig(
-    level=logging.DEBUG,
-    format="%(asctime)s|%(levelname)s|%(message)s"
+    level=config['logging_level'],
+    format="%(asctime)s|%(levelname)s|%(message)s",
     )
+
+known_actions = [
+    'fullavg', 'landavg', 'nonlandavg', 'cardinfo'
+]
 
 # Initiate discord client
 client = discord.Client()
@@ -25,20 +29,37 @@ async def on_message(message):
 
     if message.content.startswith(config['call_name']):
 
-        # Handle requests for land averages
-        if message.content.split()[1] == 'landavg':
-            deck_mage = mage.Mage(message.content, config)
-            mage_response = deck_mage.get_land_average()
 
-        if message.content.split()[1] == 'nonlandavg':
-            deck_mage = mage.Mage(message.content, config)
-            mage_response = deck_mage.get_specific_average()
+        try:
 
-        if message.content.split()[1] == 'fullavg':
-            deck_mage = mage.Mage(message.content, config)
-            mage_response = deck_mage.get_land_average() + '\n\n'
-            mage_response += deck_mage.get_specific_average() + '\n\n'
-            mage_response += deck_mage.get_sample_hands()
+            logging.info(f'Request {message.content.split()[2]} initiated by {message.author}')
+            logging.debug(f'Request: {message.content}')
+
+            # Handle requests for land averages
+            if message.content.split()[1] == 'landavg':
+                deck_mage = mage.Mage(message.content, config)
+                mage_response = deck_mage.get_land_average()
+
+            if message.content.split()[1] == 'nonlandavg':
+                deck_mage = mage.Mage(message.content, config)
+                mage_response = deck_mage.get_specific_average()
+
+            if message.content.split()[1] == 'fullavg':
+                deck_mage = mage.Mage(message.content, config)
+                mage_response = deck_mage.get_land_average() + '\n\n'
+                mage_response += deck_mage.get_specific_average() + '\n\n'
+                mage_response += deck_mage.get_sample_hands()
+
+            if message.content.split()[1] == 'cardinfo':
+                deck_mage = mage.Mage(message.content, config)
+                mage_response = deck_mage.get_card_info()
+
+        except: # Throw the error into the logs and carry on
+            logging.exception("Error caught!")
+            mage_response = f'Error processing request, I am sorry!'
+
+                    
+        
 
         await message.channel.send(mage_response)
 
